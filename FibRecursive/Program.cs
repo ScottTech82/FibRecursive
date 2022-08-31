15c57c58fa1819e12e21d6aef3f0611b0cc23099
Josh Scott <joshscott82@gmail.com>
2022-08-31 11:09:07 -0400

    Add project files.

---
+++ FibRecursive/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Reflection.Emit;
+
+Console.WriteLine("Fib Recursive!");
+
+
+fib(1, 1);
+
+void fib(int a, int b)
+{
+    var c = a + b; // creating c to print later
+    if (c > 100) return;
+    Console.WriteLine($"{c}, ");
+    fib(b, c);
+}
